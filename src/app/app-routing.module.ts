f5f28e12da18349836d8b6e4a5092ac12778fe63
dayamori <morochochoezdayana@gmail.com>
2023-01-18 00:06:07 -0500

    esqueleto actualizado + pantalla de Servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { landingComponent } from './Landing/landing.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PreciosOfertasComponent } from './precios-ofertas/precios-ofertas.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { CrudComponent } from './sistema/crud/crud.component';
 
 const routes: Routes = [
   {path:'', component: landingComponent},
+  {path:'servicios', component: ServiciosComponent},
+  {path:'precios', component: PreciosOfertasComponent},
+  {path:'contactos', component: ContactosComponent},
+  {path:'crud', component: CrudComponent}
 ];
 
 @NgModule({
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private router: Router, private dialogRef:
+    MatDialogRef<LoginComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private _snackBar?: MatSnackBar) { }
+}
